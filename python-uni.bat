6235b168e852fd6f6bee780a6abc2534a8a98020
thehappycheese <the.nicholas.archer@gmail.com>
2013-09-04 18:52:00 +0800

    Some styling

--- python-uni.bat
+++ python-uni.bat
@@ -1,1 +1,2 @@
-C:\Python31\python.exe -m http.server 80
+start C:\Users\14282850\AppData\Local\Google\Chrome\Application\chrome.exe http://127.0.0.1:8080/
+C:\Python31\python.exe -m http.server 8080
