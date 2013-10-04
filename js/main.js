5a69149b258307a81991f71cb4f86c35312ff6b2
Nicholas Archer <the.nicholas.archer@gmail.com>
2013-10-05 00:53:41 +0800

    Basic Renderer

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ document.title = "BeamCAD";
 var ui = domScrobble(document.body);
 
 var eq1 = "sin(x)";
-var eq2 = "5*(10+x)+(30+(40-x))/2";
+var eq2 = "k_ou/{10+1}";
 
 
 
@@ -37,34 +37,126 @@ function equation(exp){
 	
 	
 	result.appendChild(md(t));
-	
+	console.log(t)
 	return result
 
 }
 function md(t){
-	var result = document.createElement('div');
+	
+	
 	if(typeof t == "object"){
+		
+		
+		var result = document.createElement('table');
+		result.cellPadding = 0;
+		result.cellSpacing = 0;
+		var row = document.createElement('tr');
 		var opp = t[0];
+		
+		
 		if(opp=="()"){
-			var d = document.createElement('div');
-			d.className="opp"
+		
+			var d = document.createElement('td');
 			d.appendChild(md(t[1]));
-			result.appendChild(d);
-		}else {
-			var d = document.createElement('div');
+			d.className = "bracket";
+			row.appendChild(d);
+			result.appendChild(row);
 			
-			d.className="inline";
+		}else if(opp=="{}"){
+		
+			var d = document.createElement('td');
+			d.appendChild(md(t[1]));
+			row.appendChild(d);
+			result.appendChild(row);
 			
-			if(opp=="+" || opp=="-"){
-				d.className="bracket"
+		}else if(opp=="*" || opp=="+" || opp=="-") {
+			
+			var d = document.createElement('td');
+			d.className = "mathcell";
+			d.appendChild(md(t[1]));
+			row.appendChild(d);
+			
+			var d = document.createElement('td');
+			d.className = "mathcell";
+			
+			if(t[0]=="*"){
+				d.innerHTML+="&times;";
+			}else{
+				d.appendChild(document.createTextNode(t[0]));
 			}
-			d.appendChild(md(t[1]));
-			d.appendChild(document.createTextNode(t[0]));
+			row.appendChild(d);
+			
+			var d = document.createElement('td');
+			d.className = "mathcell";
 			d.appendChild(md(t[2]));
-			result.appendChild(d);
+			row.appendChild(d);
+			
+			result.appendChild(row);
+			
+		}else if(opp=="/") {
+			
+			var d = document.createElement('td');
+			d.className = "mathcell math-numerator";
+			d.borderBottom = "1px solid black"
+			d.appendChild(md(t[1]));
+			row.appendChild(d);
+			
+			row.appendChild(d);
+			result.appendChild(row);
+			
+			
+			
+			var row2 = document.createElement('tr');
+			
+			var d = document.createElement('td');
+			d.appendChild(md(t[2]));
+			d.className = "mathcell";
+			
+			row2.appendChild(d);
+			
+			result.appendChild(row2);
+			
+		}else if(opp == "_"){
+			var d = document.createElement('td');
+			d.className = "mathcell";
+			d.appendChild(md(t[1]));
+			row.appendChild(d);
+			
+			
+			
+			var span = document.createElement('span');
+			span.className = "math-subscript";
+			span.appendChild(md(t[2]));
+			
+			
+			var d = document.createElement('td');
+			d.className = "mathcell";
+			d.appendChild(span);
+			row.appendChild(d);
+			
+			result.appendChild(row);
 		}
 	}else{
-		result.appendChild(document.createTextNode(t));
+		//row.appendChild(document.createTextNode(t));
+		return document.createTextNode(t)
 	}
 	return result;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
