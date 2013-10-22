146b124640d1fc50b5bf98d5edeab768b76db13a
Nicholas Archer <the.nicholas.archer@gmail.com>
2013-10-22 11:36:19 +0800

    Added Tab System

--- js/lib/nArimsys/nArimsys.js
+++ js/lib/nArimsys/nArimsys.js
@@ -1,4 +1,53 @@
 
+function Arimsys (database){
+	this.dat = database;
+	
+	
+	this.getAvaliable = function(){
+		var i;
+		var result = [];
+		this.dat().each(function(r){
+			if(r.prereq==="" || this.checkPrerequisite(r.prereq)){
+				result.push(r.id);
+			}
+		}.bind(this));
+		return result;
+	}.bind(this);
+	
+	this.checkPrerequisite = function(str){
+		var prereq = prereqpegjs.parse(str);
+		return this.check(prereq);
+	}.bind(this);
+	
+	
+	
+	// ===== Recursive Prereuisite decomposition =====
+	this.check = function(exp){
+		var funcname = exp.shift()
+		return this[funcname].apply(this,exp)
+	}.bind(this);
+	
+	
+	// ===== PREREQUISITE OPPERATORS =====
+	this.has = function(varname){
+		var val = this.dat({"id":varname}).first().value;
+		return (val!==null && val!==undefined && val!=="")
+	}.bind(this);
+	this.and = function(left, right){
+		return this.check(left) && this.check(right);
+	}
+	this.or = function(left, right){
+		return this.check(left) || this.check(right);
+	}
+	this.not = function(right){
+		return !this.check(right)
+	}.bind(this);
+	
+	this.eq = function(left, right){
+		var val = this.dat({"id":left}).first().value
+		return val == right;
+	}
+}
 
 
 
--- js/main.js
+++ js/main.js
@@ -12,131 +12,52 @@ var ui = DOMTools.scrobble(document.body);
 
 
 
-//ui.mainspace.innerHTML = "";
-
-
-var tab = new nTable(10,2);
-
-var asstab = new nTable(1,1);
-
-
-
-ui.mainbar.appendChild(tab.dom);
-ui.calcbox.appendChild(asstab.dom);
+var mainview = new view(document.body);
 
 
 
 
-
-var dat = data_AS3600Variables;
-var arim = new Arimsys(dat);
-
-rendertables(["name","notation","value","unit"]);
-function rendertables (headings){
-	tab.init(dat().count()+1,headings.length);
-	var cell = tab.getCell(0,0).setValues([headings]);
+function view(dom){
+	this.dom = dom;
+	this.ui = {};
+	this.init = function(){
+		this.ui = DOMTools.scrobble(dom);
+		this.ui.tabbut0.onclick = this.changeTab;
+		this.ui.tabbut1.onclick = this.changeTab;
+		this.ui.tabbut2.onclick = this.changeTab;
+		this.ui.tabbut3.onclick = this.changeTab;
+	}.bind(this);
+	
+	this.changeTab = function(e){
+		console.log(e.target.id);
+		this.ui.tabbut0.className = "maintab";
+		this.ui.tabbut1.className = "maintab";
+		this.ui.tabbut2.className = "maintab";
+		this.ui.tabbut3.className = "maintab";
 		
-	// TODO: Get range object from table, apply formatting options to range;
-	do{
-		cell.dom.className = "heading";
-		cell = cell.getRight();
-	}while(cell!=undefined)
-	// TODO: Change this awful method of math-jaxing things
-	cell = tab.getCell(1,1);
-	do{
-		cell.convertToMathJax()
-		cell = cell.getDown();
-	}while(cell)
-	
-	var x = 0;
-	var y = 1;
-	dat().order("name").each(function(r){
-
-		headings.forEach(function(heading){
-			tab.getCell(y,x).setValue(r[heading]).dom.title = r["description"];
-			x++;
-		})
-		x=0;
-		y++;
-	});
-	
-
-	var av = arim.getAvaliable();
-	console.log(av)
-	asstab.init(av.length+1,2);
-	asstab.getCell(0,0).merge(1,2).setValue("Possible to Find:").dom.className = "heading";
-	cell = asstab.getCell(1,0);
-	var butcell,butt;
-	var i  = 0;
-	do{
-		cell.setValue(dat({"id":av[i]}).first().notation).convertToMathJax();
+		this.ui.tab0.style.display = "none";
+		this.ui.tab1.style.display = "none";
+		this.ui.tab2.style.display = "none";
+		this.ui.tab3.style.display = "none";
 		
-		butt = document.createElement("button");
-		butt.innerHTML =  "calculate it";
-		butt.vid = av[i];
-		butt.onclick = function(){
-			dat({"id":this.vid}).first().value = "###"
-			rendertables(["name","notation","value","unit"]);
-		}.bind(butt);
-		butcell = cell.getRight();
-		butcell.dom.appendChild(butt);
-		
-		i++
-		cell = cell.getDown();
-	}while (cell);
-	
-	MathJax.Hub.Queue(["Typeset",MathJax.Hub,document.body]);
-	
-}
-
-function Arimsys (database){
-	this.dat = database;
-	
-	
-	this.getAvaliable = function(){
-		var i;
-		var result = [];
-		this.dat().each(function(r){
-			if(r.prereq==="" || this.checkPrerequisite(r.prereq)){
-				result.push(r.id);
-			}
-		}.bind(this));
-		return result;
+		e.target.className += " selected";
+		switch(e.target.id){
+			case "tabbut0":
+				this.ui.tab0.style.display = "block";
+				break;
+			case "tabbut1":
+				this.ui.tab1.style.display = "block";
+				break;
+			case "tabbut2":
+				this.ui.tab2.style.display = "block";
+				break;
+			case "tabbut3":
+				this.ui.tab3.style.display = "block";
+				break;
+		}
 	}.bind(this);
 	
-	this.checkPrerequisite = function(str){
-		var prereq = prereqpegjs.parse(str);
-		return this.check(prereq);
-	}.bind(this);
-	
-	
-	
-	// ===== Recursive Prereuisite decomposition =====
-	this.check = function(exp){
-		var funcname = exp.shift()
-		return this[funcname].apply(this,exp)
-	}.bind(this);
-	
-	
-	// ===== PREREQUISITE OPPERATORS =====
-	this.has = function(varname){
-		var val = this.dat({"id":varname}).first().value;
-		return (val!==null && val!==undefined && val!=="")
-	}.bind(this);
-	this.and = function(left, right){
-		return this.check(left) && this.check(right);
-	}
-	this.or = function(left, right){
-		return this.check(left) || this.check(right);
-	}
-	this.not = function(right){
-		return !this.check(right)
-	}.bind(this);
-	
-	this.eq = function(left, right){
-		var val = this.dat({"id":left}).first().value
-		return val == right;
-	}
+	this.init();
 }
 
 
